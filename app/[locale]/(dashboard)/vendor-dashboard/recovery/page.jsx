a56777ffebb75d22933e7096b3856cdef2d41e50
Felipe Machado <66793977+Ferrama@users.noreply.github.com>
2024-01-11 09:07:59 -0300

    remove import

--- app/[locale]/(dashboard)/vendor-dashboard/recovery/page.jsx
+++ app/[locale]/(dashboard)/vendor-dashboard/recovery/page.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import DashboardPage from "../../../../components/dashboard/vendor-dashboard/recovery";
+
 
 export const metadata = {
   title: "Vendor Recovery || HolidayAir",
@@ -9,7 +9,7 @@ export const metadata = {
 export default function page() {
   return (
     <>
-      <DashboardPage />
+
     </>
   );
 }
