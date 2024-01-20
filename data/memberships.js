bda7a427bd7ceafce9ceb09d0c82618dbbefd343
Felipe Machado <66793977+Ferrama@users.noreply.github.com>
2024-01-20 10:54:40 -0300

    update memberships

---
+++ data/memberships.js
@@ -0,0 +1,20 @@
+const memberships = [
+    {
+        id: 'none',
+        price: 'xxxxx',
+        callToAction: () => { },
+        features: [
+            {
+                id: 'free-seat',
+                value: false,
+            },
+            {
+                id: 'free-seat',
+                value: false,
+            }, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+    },
+    {},
+    {},
+    {},
+    {}
+]
