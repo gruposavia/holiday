8dc508c5fe7ae6ffddc5aa3a7fc752ffc83447c0
Felipe Machado <66793977+Ferrama@users.noreply.github.com>
2024-01-23 19:06:40 -0300

    update

--- pages/api/getPrivacyTerms.js
+++ pages/api/getPrivacyTerms.js
@@ -1,5 +1,6 @@
-// pages/api/getPrivacyTerms.js
-import fs from 'fs/promises';
+
+import fs from 'fs';
+import { promisify } from 'util';
 import matter from 'gray-matter';
 import path from 'path';
 import { defaultLocale } from '@/i18nConfig';
@@ -7,12 +8,14 @@ import { defaultLocale } from '@/i18nConfig';
 const baseDirectory = process.cwd();
 const postsDirectory = path.join(baseDirectory, 'mdx', 'terms');
 
+const readFileAsync = promisify(fs.readFile);
+
 export default async function handler(req, res) {
   const { locale = defaultLocale } = req.query;
 
   try {
     const fullPath = path.join(postsDirectory, `${locale}.md`);
-    const fileContents = await fs.readFile(fullPath, 'utf8');
+    const fileContents = await readFileAsync(fullPath, 'utf8');
     const matterResult = matter(fileContents);
 
     res.status(200).json({ content: matterResult.content });
