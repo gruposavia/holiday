2144e41f4dcd7faeb6a3e406564ee14a4fcdc849
Felipe Machado <66793977+Ferrama@users.noreply.github.com>
2024-02-26 09:45:09 -0300

    fix build

--- app/[locale]/(others)/404/page.jsx
+++ app/[locale]/(others)/404/page.jsx
@@ -2,12 +2,10 @@ import dynamic from "next/dynamic";
 import CallToActions from "@/components/common/CallToActions";
 import Header10 from "@/components/header/header-10";
 import DefaultFooter from "@/components/footer/default";
-import NotFound from "@/components/common/NotFound";
-import initTranslations from '../../../i18n';
-import TranslationsProvider from '../../../../components/TranslationProvider';
-import {
-  i18nNamespaces,
-} from "@/utils/translationsContansts";
+import NotFound from "@/components/notFound/NotFound";
+import initTranslations from "../../../i18n";
+import TranslationsProvider from "../../../../components/TranslationProvider";
+import { i18nNamespaces } from "@/utils/translationsContansts";
 export const metadata = {
   title: "404 || HolidayAir",
   description: "HolidayAir",
@@ -16,27 +14,31 @@ export const metadata = {
 const index = async ({ params: { locale } }) => {
   const { t, resources } = await initTranslations(locale, i18nNamespaces);
   return (
-    t && locale && <TranslationsProvider 
-    namespaces={i18nNamespaces}
-    locale={locale}
-    resources={resources}>
-      {/* End Page Title */}
+    t &&
+    locale && (
+      <TranslationsProvider
+        namespaces={i18nNamespaces}
+        locale={locale}
+        resources={resources}
+      >
+        {/* End Page Title */}
 
-      <div className="header-margin"></div>
-      {/* header top margin */}
+        <div className="header-margin"></div>
+        {/* header top margin */}
 
-      <Header10 locale={locale} />
-      {/* End Header 1 */}
+        <Header10 locale={locale} />
+        {/* End Header 1 */}
 
-      <NotFound t={t} locale={locale}/>
-      {/* End 404 section */}
+        <NotFound t={t} locale={locale} />
+        {/* End 404 section */}
 
-      <CallToActions />
-      {/* End Call To Actions Section */}
+        <CallToActions />
+        {/* End Call To Actions Section */}
 
-      <DefaultFooter t={t}/>
-      {/* End Call To Actions Section */}
-    </TranslationsProvider>
+        <DefaultFooter t={t} />
+        {/* End Call To Actions Section */}
+      </TranslationsProvider>
+    )
   );
 };
 
